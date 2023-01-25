3213d4bde37e2f08fbeddf509031207dc1c9d602
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-25 17:48:05 -0300

    Add gnark_backend setup

---
+++ gnark_backend/cmd/wasm/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/test"
+
+	"github.com/consensys/gnark/frontend"
+)
+
+// In this example we show how to use PLONK with KZG commitments. The circuit that is
+// showed here is the same as in ../exponentiate.
+
+// Circuit y == x**e
+// only the bitSize least significant bits of e are used
+type Circuit struct {
+	// tagging a variable is optional
+	// default uses variable name and secret visibility.
+	X frontend.Variable `gnark:",public"`
+	Y frontend.Variable `gnark:",public"`
+
+	E frontend.Variable
+}
+
+// Define declares the circuit's constraints
+// y == x**e
+func (circuit *Circuit) Define(api frontend.API) error {
+
+	// number of bits of exponent
+	const bitSize = 4000
+
+	// specify constraints
+	output := frontend.Variable(1)
+	bits := api.ToBinary(circuit.E, bitSize)
+
+	for i := 0; i < len(bits); i++ {
+		// api.Println(fmt.Sprintf("e[%d]", i), bits[i]) // we may print a variable for testing and / or debugging purposes
+
+		if i != 0 {
+			output = api.Mul(output, output)
+		}
+		multiply := api.Mul(output, circuit.X)
+		output = api.Select(bits[len(bits)-1-i], multiply, output)
+
+	}
+
+	api.AssertIsEqual(circuit.Y, output)
+
+	return nil
+}
+
+func main() {
+
+	var circuit Circuit
+
+	// // building the circuit...
+	ccs, err := frontend.Compile(ecc.BN254, scs.NewBuilder, &circuit)
+	if err != nil {
+		fmt.Println("circuit compilation error")
+	}
+
+	// create the necessary data for KZG.
+	// This is a toy example, normally the trusted setup to build ZKG
+	// has been ran before.
+	// The size of the data in KZG should be the closest power of 2 bounding //
+	// above max(nbConstraints, nbVariables).
+	_r1cs := plonk.NewCS(ecc.BN254)
+	srs, err := test.NewKZGSRS(_r1cs)
+	if err != nil {
+		panic(err)
+	}
+
+	// Correct data: the proof passes
+	{
+		// Witnesses instantiation. Witness is known only by the prover,
+		// while public w is a public data known by the verifier.
+		var w Circuit
+		w.X = 2
+		w.E = 2
+		w.Y = 4
+
+		witnessFull, err := frontend.NewWitness(&w, ecc.BN254)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		witnessPublic, err := frontend.NewWitness(&w, ecc.BN254, frontend.PublicOnly())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// public data consists the polynomials describing the constants involved
+		// in the constraints, the polynomial describing the permutation ("grand
+		// product argument"), and the FFT domains.
+		pk, vk, err := plonk.Setup(ccs, srs)
+		//_, err := plonk.Setup(r1cs, kate, &publicWitness)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proof, err := plonk.Prove(ccs, pk, witnessFull)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = plonk.Verify(proof, vk, witnessPublic)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// Wrong data: the proof fails
+	{
+		// Witnesses instantiation. Witness is known only by the prover,
+		// while public w is a public data known by the verifier.
+		var w, pW Circuit
+		w.X = 2
+		w.E = 12
+		w.Y = 4096
+
+		pW.X = 3
+		pW.Y = 4096
+
+		witnessFull, err := frontend.NewWitness(&w, ecc.BN254)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		witnessPublic, err := frontend.NewWitness(&pW, ecc.BN254, frontend.PublicOnly())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// public data consists the polynomials describing the constants involved
+		// in the constraints, the polynomial describing the permutation ("grand
+		// product argument"), and the FFT domains.
+		pk, vk, err := plonk.Setup(ccs, srs)
+		//_, err := plonk.Setup(r1cs, kate, &publicWitness)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proof, err := plonk.Prove(ccs, pk, witnessFull)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = plonk.Verify(proof, vk, witnessPublic)
+		if err == nil {
+			log.Fatal("Error: wrong proof is accepted")
+		}
+	}
+}
