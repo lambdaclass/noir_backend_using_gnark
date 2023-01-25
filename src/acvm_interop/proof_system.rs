259a6dd35ec60260d373a2c1c12f80172129ee98
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-25 18:15:55 -0300

    Fill ProofSystemCompiler implementation

--- src/acvm_interop/proof_system.rs
+++ src/acvm_interop/proof_system.rs
@@ -1,15 +1,30 @@
 use acvm::acir::{circuit::Circuit, native_types::Witness, BlackBoxFunc};
 use acvm::{FieldElement, Language, ProofSystemCompiler};
 
+use crate::gnark_backend_glue as gnark_backend;
+
 use super::Gnark;
 
 impl ProofSystemCompiler for Gnark {
     fn np_language(&self) -> Language {
-        todo!()
+        Language::PLONKCSat { width: 3 }
     }
 
     fn blackbox_function_supported(&self, opcode: &BlackBoxFunc) -> bool {
-        todo!()
+        match opcode {
+            BlackBoxFunc::AES => false,
+            BlackBoxFunc::AND => false,
+            BlackBoxFunc::XOR => false,
+            BlackBoxFunc::RANGE => false,
+            BlackBoxFunc::SHA256 => false,
+            BlackBoxFunc::Blake2s => false,
+            BlackBoxFunc::MerkleMembership => false,
+            BlackBoxFunc::SchnorrVerify => false,
+            BlackBoxFunc::Pedersen => false,
+            BlackBoxFunc::HashToField128Security => false,
+            BlackBoxFunc::EcdsaSecp256k1 => false,
+            BlackBoxFunc::FixedBaseScalarMul => false,
+        }
     }
 
     fn prove_with_meta(
@@ -17,7 +32,9 @@ impl ProofSystemCompiler for Gnark {
         circuit: Circuit,
         witness_values: std::collections::BTreeMap<Witness, FieldElement>,
     ) -> Vec<u8> {
-        todo!()
+        // TODO: modify gnark serializer to accept the BTreeMap
+        let values: Vec<FieldElement> = witness_values.values().copied().collect();
+        gnark_backend::prove(circuit, values.as_slice())
     }
 
     fn verify_from_cs(
@@ -26,10 +43,10 @@ impl ProofSystemCompiler for Gnark {
         public_inputs: Vec<FieldElement>,
         circuit: Circuit,
     ) -> bool {
-        todo!()
+        gnark_backend::verify(circuit, proof, &public_inputs)
     }
 
-    fn get_exact_circuit_size(&self, circuit: Circuit) -> u32 {
+    fn get_exact_circuit_size(&self, _circuit: Circuit) -> u32 {
         todo!()
     }
 }
