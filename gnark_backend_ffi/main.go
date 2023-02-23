59dad498274ba7dacffb744771786a78a3db583c
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-23 15:53:48 -0300

    Implement `ProveWithPK` in the Go backend

--- gnark_backend_ffi/main.go
+++ gnark_backend_ffi/main.go
@@ -4,9 +4,11 @@ import "C"
 import (
 	"bytes"
 	"fmt"
+	"log"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
+	cs_bls12381 "github.com/consensys/gnark/constraint/bls12-381"
 )
 
 //export Prove
@@ -21,6 +23,36 @@ func Prove(rawr1cs string) *C.char {
 	proof.WriteRawTo(&serialized_proof)
 
 	return C.CString(rawr1cs)
+}
+
+//export ProveWithPK
+func ProveWithPK(rawR1CS string, provingKey string) *C.char {
+	// Create R1CS.
+	r1cs := cs_bls12381.NewR1CS(1)
+
+	// Add variables.
+
+	// Add constraints.
+
+	// Deserialize proving key.
+	pk := groth16.NewProvingKey(r1cs.CurveID())
+	_, err := pk.ReadFrom(bytes.NewReader([]byte(provingKey)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prove.
+	proof, err := groth16.Prove(r1cs, pk, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Serialize proof
+	var serialized_proof bytes.Buffer
+	proof.WriteTo(&serialized_proof)
+	proof_string := serialized_proof.String()
+
+	return C.CString(proof_string)
 }
 
 //export Verify
