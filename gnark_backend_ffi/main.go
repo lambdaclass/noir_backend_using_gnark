429f18ff4595a3be9cc7e3e919f3f9cef2376b2a
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-02-23 14:46:18 -0300

    Setup glue backend (#8)
    * Setup WASM API
    
    * Add `acir_to_r1cs` module
    
    * Add `helpers` module
    
    * Add modules to `mod.rs`
    
    * Update deps and add features
    
    * cargo fmt
    
    * Fix import
    
    * Update `acvm` import
    
    * Remove generics
    
    * Remove `helpers` module
    
    * Implement serializer for `RawR1CS` & `RawGate`
    
    * Add `ark-serialize`
    
    * Add `hex`
    
    * Add conditional compilation
    
    * Refactor naming & namespace
    
    * Update `prove_with_meta`
    
    * Update `verify_from_cs`
    
    * Update comment
    
    * Rename WASM gnark backend
    
    * Add FFI gnark backend
    
    * Update parameter type
    
    * Add `anyhow::Result` as import
    
    * Update `acvm`
    
    * Fix go backend deps
    
    * Add verify call to backend glue
    
    * Add newline at end of file
    
    * Add Prove call to backend glue
    
    * Add Prove input: RawR1CS as JSON
    
    * Update Prove
    Now returns string
    
    * Update Verify
    Now receives RawR1CS and Proof
    
    * Add GoString::from_cstring
    
    * Change c_msg to c_str
    
    * Update GoString field names
    
    * Update verify
    Now bails if it returns an invalid bool
    
    * Fix imports
    
    * Change unwrap to ?
    
    * Change datatype i64 to u64 for GoString length
    
    * Update GoString length field
    Now it is a usize
    
    ---------
    
    Co-authored-by: Iván Litteri <ilitteri@fi.uba.ar>

--- gnark_backend_ffi/main.go
+++ gnark_backend_ffi/main.go
@@ -3,24 +3,31 @@ package main
 import "C"
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 )
 
-//export prove
-func prove() []byte {
+//export Prove
+func Prove(rawr1cs string) *C.char {
+
+	fmt.Printf("rawr1cs: %v\n", rawr1cs)
+
 	var serialized_proof bytes.Buffer
 
 	proof := groth16.NewProof(ecc.BLS12_381)
 
 	proof.WriteRawTo(&serialized_proof)
 
-	return serialized_proof.Bytes()
+	return C.CString(rawr1cs)
 }
 
-//export verify
-func verify() bool {
+//export Verify
+func Verify(rawr1cs string, proof string) bool {
+
+	fmt.Printf("rawr1cs: %v\n", rawr1cs)
+	fmt.Printf("proof: %v\n", proof)
 	return false
 }
 
--- src/gnark_backend_glue/mod.rs
+++ src/gnark_backend_glue/mod.rs
@@ -1,8 +1,12 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_uchar};
+
 use acvm::{acir::circuit::Circuit, FieldElement};
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 mod acir_to_r1cs;
 mod serialize;
+use crate::gnark_backend_glue::acir_to_r1cs::RawR1CS;
 
 // Arkworks's types are generic for `Field` but Noir's types are concrete and
 // its value depends on the feature flag.
@@ -28,10 +32,79 @@ cfg_if::cfg_if! {
     }
 }
 
-pub fn prove(_circuit: Circuit, _values: Vec<FieldElement>) -> Result<Vec<u8>> {
-    todo!()
+extern "C" {
+    fn Verify(rawr1cs: GoString, proof: GoString) -> c_uchar;
+    fn Prove(rawr1cs: GoString) -> *const c_char;
 }
 
-pub fn verify(_circuit: Circuit, _proof: &[u8], _public_inputs: &[FieldElement]) -> Result<bool> {
-    todo!()
+#[derive(Debug)]
+#[repr(C)]
+struct GoString {
+    ptr: *const c_char,
+    length: usize,
+}
+
+impl GoString {
+    pub fn from_cstring(c_str: &CString) -> GoString {
+        let ptr = c_str.as_ptr();
+        let length = c_str.as_bytes().len();
+        GoString { ptr, length }
+    }
+}
+
+pub fn prove(circuit: Circuit, values: Vec<FieldElement>) -> Result<Vec<u8>> {
+    let rawr1cs = RawR1CS::new(circuit, values)?;
+
+    // Serialize to json and then convert to GoString
+    let serialized_rawr1cs = serde_json::to_string(&rawr1cs)?;
+    let c_str = CString::new(serialized_rawr1cs)?;
+    let go_string_rawr1cs = GoString::from_cstring(&c_str);
+
+    let result: *const c_char = unsafe { Prove(go_string_rawr1cs) };
+    let c_str = unsafe { CStr::from_ptr(result) };
+    let bytes = c_str.to_str()?.as_bytes();
+
+    Ok(bytes.to_vec())
+}
+
+pub fn verify(circuit: Circuit, proof: &[u8], public_inputs: &[FieldElement]) -> Result<bool> {
+    let rawr1cs = RawR1CS::new(circuit, public_inputs.to_vec())?;
+
+    // Serialize to json and then convert to GoString
+    let rawr1cs_json = serde_json::to_string(&rawr1cs)?;
+    let c_str = CString::new(rawr1cs_json)?;
+    let go_string_rawr1cs = GoString::from_cstring(&c_str);
+
+    let serialized_proof = std::str::from_utf8(proof)?.to_string();
+    let c_str = CString::new(serialized_proof)?;
+    let go_string_proof = GoString::from_cstring(&c_str);
+
+    let result = unsafe { Verify(go_string_rawr1cs, go_string_proof) };
+    match result {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => bail!("Verify did not return a valid bool"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+
+    #[test]
+    fn verify_should_return_false() {
+        let result = verify(Circuit::default(), &[65, 66, 67], &[]).unwrap();
+        assert!(!result);
+    }
+
+    #[test]
+    fn prove_should_call_go_backend() {
+        let result = prove(Circuit::default(), vec![]).unwrap();
+
+        assert_eq!(
+            std::str::from_utf8(&result).unwrap(),
+            "{\"gates\":[],\"public_inputs\":[],\"values\":[],\"num_variables\":1}"
+        );
+    }
 }
