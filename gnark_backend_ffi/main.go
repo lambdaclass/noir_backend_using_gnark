ed78236a15153802902d0d0a0ab98abce0a2a705
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-23 18:21:23 -0300

    Implement `Preprocess` in the Go backend

--- gnark_backend_ffi/main.go
+++ gnark_backend_ffi/main.go
@@ -4,9 +4,11 @@ import "C"
 import (
 	"bytes"
 	"fmt"
+	"log"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
+	cs_bls12381 "github.com/consensys/gnark/constraint/bls12-381"
 )
 
 //export Prove
@@ -29,6 +31,34 @@ func Verify(rawr1cs string, proof string) bool {
 	fmt.Printf("rawr1cs: %v\n", rawr1cs)
 	fmt.Printf("proof: %v\n", proof)
 	return false
+}
+
+//export Preprocess
+func Preprocess(rawR1CS string) (*C.char, *C.char) {
+	// Create R1CS.
+	r1cs := cs_bls12381.NewR1CS(1)
+
+	// Add variables.
+
+	// Add constraints.
+
+	// Setup.
+	pk, vk, err := groth16.Setup(r1cs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Serialize proving key.
+	var serialized_pk bytes.Buffer
+	pk.WriteTo(&serialized_pk)
+	pk_string := serialized_pk.String()
+
+	// Serialize verifying key.
+	var serialized_vk bytes.Buffer
+	vk.WriteTo(&serialized_vk)
+	vk_string := serialized_vk.String()
+
+	return C.CString(pk_string), C.CString(vk_string)
 }
 
 func main() {}
