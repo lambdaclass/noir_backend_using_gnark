b21e02246390b3e120947b541e6ae627f4d06404
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-02-24 10:53:56 -0300

    Implement `ProveWithPK` in the Go backend (#14)

--- gnark_backend_ffi/main.go
+++ gnark_backend_ffi/main.go
@@ -41,6 +41,36 @@ func ProveWithMeta(rawR1CS string) *C.char {
 	return C.CString(proof_string)
 }
 
+//export ProveWithPK
+func ProveWithPK(rawR1CS string, provingKey string) *C.char {
+	// Create R1CS.
+	r1cs := cs_bls12381.NewR1CS(1)
+
+	// Add variables.
+
+	// Add constraints.
+
+	// Deserialize proving key.
+	pk := groth16.NewProvingKey(r1cs.CurveID())
+	_, err := pk.ReadFrom(bytes.NewReader([]byte(provingKey)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prove.
+	proof, err := groth16.Prove(r1cs, pk, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Serialize proof
+	var serialized_proof bytes.Buffer
+	proof.WriteTo(&serialized_proof)
+	proof_string := serialized_proof.String()
+
+	return C.CString(proof_string)
+}
+
 //export Verify
 func Verify(rawr1cs string, proof string) bool {
 
