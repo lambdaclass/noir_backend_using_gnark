147dc4dc835801132b685d045d88df4b5196f7cb
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-24 15:05:48 -0300

    Refactor GoString::from_cstring

--- src/gnark_backend_glue/mod.rs
+++ src/gnark_backend_glue/mod.rs
@@ -48,11 +48,13 @@ struct GoString {
     length: usize,
 }
 
-impl GoString {
-    pub fn from_cstring(c_str: &CString) -> GoString {
-        let ptr = c_str.as_ptr();
-        let length = c_str.as_bytes().len();
-        GoString { ptr, length }
+impl TryFrom<&CString> for GoString {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &CString) -> std::result::Result<Self, Self::Error> {
+        let ptr = value.as_ptr();
+        let length = value.as_bytes().len();
+        Ok(Self { ptr, length })
     }
 }
 
@@ -62,7 +64,7 @@ pub fn prove_with_meta(circuit: Circuit, values: Vec<FieldElement>) -> Result<Ve
     // Serialize to json and then convert to GoString
     let serialized_rawr1cs = serde_json::to_string(&rawr1cs)?;
     let c_str = CString::new(serialized_rawr1cs)?;
-    let go_string_rawr1cs = GoString::from_cstring(&c_str);
+    let go_string_rawr1cs = GoString::try_from(&c_str)?;
 
     let result: *const c_char = unsafe { ProveWithMeta(go_string_rawr1cs) };
     let c_str = unsafe { CStr::from_ptr(result) };
@@ -81,11 +83,11 @@ pub fn prove_with_pk(
     // Serialize to json and then convert to GoString
     let rawr1cs_json = serde_json::to_string(&rawr1cs)?;
     let rawr1cs_c_str = CString::new(rawr1cs_json)?;
-    let rawr1cs_go_string = GoString::from_cstring(&rawr1cs_c_str);
+    let rawr1cs_go_string = GoString::try_from(&rawr1cs_c_str)?;
 
     let proving_key_serialized = String::from_utf8(proving_key.to_vec())?;
     let proving_key_c_str = CString::new(proving_key_serialized)?;
-    let proving_key_go_string = GoString::from_cstring(&proving_key_c_str);
+    let proving_key_go_string = GoString::try_from(&proving_key_c_str)?;
 
     let result: *const c_char = unsafe { ProveWithPK(rawr1cs_go_string, proving_key_go_string) };
     let c_str = unsafe { CStr::from_ptr(result) };
@@ -104,11 +106,11 @@ pub fn verify_with_meta(
     // Serialize to json and then convert to GoString
     let rawr1cs_json = serde_json::to_string(&rawr1cs)?;
     let c_str = CString::new(rawr1cs_json)?;
-    let go_string_rawr1cs = GoString::from_cstring(&c_str);
+    let go_string_rawr1cs = GoString::try_from(&c_str)?;
 
     let serialized_proof = String::from_utf8(proof.to_vec())?;
     let c_str = CString::new(serialized_proof)?;
-    let go_string_proof = GoString::from_cstring(&c_str);
+    let go_string_proof = GoString::try_from(&c_str)?;
 
     let result = unsafe { VerifyWithMeta(go_string_rawr1cs, go_string_proof) };
     match result {
@@ -129,15 +131,15 @@ pub fn verify_with_vk(
     // Serialize to json and then convert to GoString
     let rawr1cs_json = serde_json::to_string(&rawr1cs)?;
     let rawr1cs_c_str = CString::new(rawr1cs_json)?;
-    let rawr1cs_go_string = GoString::from_cstring(&rawr1cs_c_str);
+    let rawr1cs_go_string = GoString::try_from(&rawr1cs_c_str)?;
 
     let proof_serialized = String::from_utf8(proof.to_vec())?;
     let proof_c_str = CString::new(proof_serialized)?;
-    let proof_go_string = GoString::from_cstring(&proof_c_str);
+    let proof_go_string = GoString::try_from(&proof_c_str)?;
 
     let verifying_key_serialized = String::from_utf8(verifying_key.to_vec())?;
     let verifying_key_c_str = CString::new(verifying_key_serialized)?;
-    let verifying_key_go_string = GoString::from_cstring(&verifying_key_c_str);
+    let verifying_key_go_string = GoString::try_from(&verifying_key_c_str)?;
 
     let result =
         unsafe { VerifyWithVK(rawr1cs_go_string, proof_go_string, verifying_key_go_string) };
@@ -152,7 +154,7 @@ pub fn get_exact_circuit_size(circuit: &Circuit) -> Result<u32> {
     // Serialize to json and then convert to GoString
     let circuit_json = serde_json::to_string(circuit)?;
     let circuit_c_str = CString::new(circuit_json)?;
-    let circuit_go_string = GoString::from_cstring(&circuit_c_str);
+    let circuit_go_string = GoString::try_from(&circuit_c_str)?;
 
     let result: c_uint = unsafe { GetExactCircuitSize(circuit_go_string) };
 
