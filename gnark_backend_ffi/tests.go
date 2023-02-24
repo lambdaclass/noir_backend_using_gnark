2d091f2ac3ebe0e915459a27c429f491b9deed4d
Iván Litteri <ilitteri@fi.uba.ar>
2023-02-24 14:37:50 -0300

    Add tests definitions

---
+++ gnark_backend_ffi/tests.go
@@ -0,0 +1,41 @@
+package tests
+
+import "testing"
+
+func TestProveWithMeta(t *testing.T) {
+	// Define rawR1CS JSON string.
+
+	// Call ProveWithMeta.
+}
+
+func TestProveWithPK(t *testing.T) {
+	// Define rawR1CS JSON string.
+
+	// Define proving key.
+
+	// Call ProveWithPK.
+}
+
+func TestVerifyWithMeta(t *testing.T) {
+	// Define rawR1CS JSON string.
+
+	// Define proof.
+
+	// Call VerifyWithMeta.
+}
+
+func TestVerifyWithVK(t *testing.T) {
+	// Define rawR1CS JSON string.
+
+	// Define proof.
+
+	// Define verifying key.
+
+	// Call VerifyWithVK.
+}
+
+func TestPreprocess(t *testing.T) {
+	// Define rawR1CS JSON string.
+
+	// Call Preprocess.
+}
