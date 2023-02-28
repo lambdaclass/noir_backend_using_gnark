ac03a481a5c0846ddff79fbefd72766eb49ce1d5
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-02-28 17:45:47 -0300

    Refactor acvm_interop (#37)
    * Refactor acvm_interop
    
    * Group imports in acvm_interop
    
    * Move imports

--- src/acvm_interop.rs
+++ src/acvm_interop.rs
@@ -1,9 +1,17 @@
-use acvm::acir::{circuit::Circuit, native_types::Witness, BlackBoxFunc};
-use acvm::{FieldElement, Language, ProofSystemCompiler};
+use acvm::acir::{
+    circuit::opcodes::BlackBoxFuncCall, circuit::Circuit, native_types::Witness, BlackBoxFunc,
+};
+use acvm::{
+    FieldElement, Language, OpcodeResolutionError, PartialWitnessGenerator, ProofSystemCompiler,
+    SmartContract,
+};
+use std::collections::BTreeMap;
 
 use crate::gnark_backend_wrapper::groth16 as gnark_backend;
 
-use super::Gnark;
+pub struct Gnark;
+
+impl acvm::Backend for Gnark {}
 
 impl ProofSystemCompiler for Gnark {
     fn np_language(&self) -> Language {
@@ -74,5 +82,37 @@ impl ProofSystemCompiler for Gnark {
         verification_key: &[u8],
     ) -> bool {
         gnark_backend::verify_with_vk(circuit, proof, &public_inputs, verification_key).unwrap()
+    }
+}
+
+impl PartialWitnessGenerator for Gnark {
+    fn solve_black_box_function_call(
+        _initial_witness: &mut BTreeMap<Witness, FieldElement>,
+        _func_call: &BlackBoxFuncCall,
+    ) -> Result<(), OpcodeResolutionError> {
+        todo!()
+    }
+}
+
+#[allow(dead_code)]
+pub struct GadgetCaller;
+
+impl GadgetCaller {
+    #[allow(dead_code)]
+    pub fn solve_blackbox_func_call(
+        _initial_witness: &mut BTreeMap<Witness, FieldElement>,
+        _gadget_call: &BlackBoxFuncCall,
+    ) -> Result<(), OpcodeResolutionError> {
+        todo!()
+    }
+}
+
+impl SmartContract for Gnark {
+    fn eth_contract_from_cs(&self, _circuit: Circuit) -> String {
+        unimplemented!("gnark does not implement an ETH contract")
+    }
+
+    fn eth_contract_from_vk(&self, _verification_key: &[u8]) -> String {
+        unimplemented!("gnark does not implement an ETH contract")
     }
 }
--- src/acvm_interop/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod proof_system;
-pub mod pwg;
-
-mod smart_contract;
-pub struct Gnark;
-
-impl acvm::Backend for Gnark {}
