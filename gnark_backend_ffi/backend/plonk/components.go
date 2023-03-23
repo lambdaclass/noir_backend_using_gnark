797a40e99c96ab2624740016c0290b15d53bcf3f
Iván Litteri <ilitteri@fi.uba.ar>
2023-03-23 13:13:49 -0300

    Improve documentation

--- gnark_backend_ffi/backend/plonk/components.go
+++ gnark_backend_ffi/backend/plonk/components.go
@@ -69,10 +69,19 @@ func findOneIndex(sparseR1CS *cs_bn254.SparseR1CS, secretVariables fr_bn254.Vect
 //
 // bitIndex is the index of the evaluated bit in the values vector.
 // sparseR1CS is the constraint system being mutated.
-// values is the vector of values and it is necessary because we're adding two
-// new values here (one for 1 - b and another one for (1 - b) * b)
-// TODO: Maybe these constraints could be reduced to one (b * b^2)
-// TODO: Make tests for this.
+// secretVariables is the vector of secret variables and it is necessary because
+// we're adding two new values here (one for 1 - b and another one for (1 - b) * b)
+// and they could not be public because they're intermediate variables and they
+// could not be internal variables because in that case we'd lose track of the
+// index of these added variables.
+//
+// It is important to note that the values vector being mutated here are the secret
+// variables (the explanation is above) and it is also important to note that this
+// component or any component should be added after calling backend.HandleValues()
+// because the order in which we add se public and the secret variables matters when
+// using Gnark (the public variables must be added first, then the secret variables)
+// and because we're adding a secret variable here we need to be sure that the
+// public and secret variables are already added to the constraint system.
 //
 // The intuition here is for the constraints to be satisfied if and only if the
 // inputs are either 1 or 0. The constraints here verifies the following:
