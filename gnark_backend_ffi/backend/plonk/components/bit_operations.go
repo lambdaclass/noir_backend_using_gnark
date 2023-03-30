69ccd6fe58c56e73e16fdb76c1b72ef8477520c8
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-03-30 10:42:12 -0300

    `Xor` component (#73)
    * Add first implementation of  gadget
    
    * Update gnark_backend_ffi/backend/plonk/components.go
    
    * Update gnark_backend_ffi/backend/plonk/components.go
    
    * Update gnark_backend_ffi/backend/plonk/components.go
    
    * Update gnark_backend_ffi/backend/plonk/components.go
    
    * Add function to get the one wire's index
    
    * Fix function signature
    
    * Improve documentation
    
    * Add secret variables instead of internal ones for the intermediate variables
    
    * Return the mutated secret variables
    
    * Refactor a function (abstract it from the ACIR)
    
    * Add tests for `assertIsBoolean` component
    
    * Add tests for bit and component
    
    * Add bit and component
    
    * Add multiple-bit and component
    
    * Update result's secret variable name
    
    * Add xor component implementation
    
    Both for bit-to-bit and multiple bits
    
    * Fix append
    
    * Add tests for single bit xor
    
    * Fix `assertIsBoolean` constraint component
    
    * Remove useless function
    
    * Update naming
    
    * Fix
    
    * Add `assertIsEqual` component
    
    * Refactor repeated code in tests
    
    * Add `assertIsEqual` component tests
    
    * Implement `add` `mul` & `assertIsEqual` components
    
    * Fix `toBinaryConversion` component
    
    * Update secret variables naming
    
    * Add `toBinaryConversion` tests
    
    * Remove print
    
    * Add missing boolean assertion when converting to binary
    
    * Improve variable labels
    
    * Fix `toBinaryConversion`
    
    * Flag to generate constraint over `and` inputs
    
    * Update `And`
    
    * Shift coefficient after every iteration
    
    * Implement `fromBinaryConversion` component
    
    * Update tests naming
    
    * Finish `And` implementation
    
    * Update `accumulator` label name
    
    * Remove needless boolean assertion
    
    It is asserted above
    
    * Document components
    
    * Cleanup components
    
    * Rename module
    
    * Doc `assertIsEqual`
    
    * Finish `Xor` & `xor` components implementations
    
    * Update doc

--- gnark_backend_ffi/backend/plonk/components/bit_operations.go
+++ gnark_backend_ffi/backend/plonk/components/bit_operations.go
@@ -57,7 +57,7 @@ func and(lhs int, rhs int, sparseR1CS *cs_bn254.SparseR1CS, secretVariables fr_b
 
 // Generates constraints for the AND operation between to values.
 // If you know beforehand that the inputs are boolean, you should use the `and`
-// function.
+// component.
 //
 // It generates 2 * ((3 * bits) + 1) + bits + (2 * bits) Plonk constraints.
 //
@@ -78,6 +78,102 @@ func And(lhs int, rhs int, bits int, sparseR1CS *cs_bn254.SparseR1CS, secretVari
 		rhsBitIndex := rhsBitsIndices[i]
 		// Inputs were constrained in the above `toBinaryConversion` calls.
 		resultBit, _secretVariables := and(lhsBitIndex, rhsBitIndex, sparseR1CS, secretVariables, false)
+		secretVariables = _secretVariables
+		resultBits[i] = resultBit
+	}
+
+	return fromBinaryConversion(resultBits, sparseR1CS, secretVariables, false)
+}
+
+// Generates constraints for the bit operation XOR between two boolean values.
+//
+// It generates one Plonk constraint with constrained inputs or three Plonk
+// constraint with unconstrained inputs.
+//
+// lhs is the index of the left hand side of the XOR operation in the values vector.
+// rhs is the index of the right hand side of the XOR operation in the values vector.
+// sparseR1CS is the constraint system being mutated.
+// unconstrainedInputs is a boolean that indicates if the inputs are constrained
+// or not. If they are not constrained, then the function will generate the
+// necessary constraints to ensure that the inputs are boolean.
+//
+// Returns a tuple with the index of the result of the operation in the secret
+// variables vector.
+func xor(lhs int, rhs int, sparseR1CS *cs_bn254.SparseR1CS, secretVariables fr_bn254.Vector, unconstrainedInputs bool) (int, fr_bn254.Vector) {
+	if unconstrainedInputs {
+		assertIsBoolean(lhs, sparseR1CS)
+		assertIsBoolean(rhs, sparseR1CS)
+	}
+
+	var xa, xb, xc int
+	var qL, qR, qO, qM1, qM2 constraint.Coeff
+
+	// -1 * a
+	qL = sparseR1CS.FromInterface(-1)
+	xa = lhs
+	// -1 * b
+	qR = sparseR1CS.FromInterface(-1)
+	xb = rhs
+	// 2 * (a * b)
+	qM1 = sparseR1CS.FromInterface(2)
+	xa = lhs
+	qM2 = sparseR1CS.One()
+	xb = rhs
+	// a + b - 2 * a * b
+	qO = sparseR1CS.FromInterface(1)
+	xc = sparseR1CS.AddSecretVariable("a + b - 2 * (a * b)")
+
+	// Add a + b - 2 * a * b to the values vector so it could be recovered with xc (the index to it).
+	var (
+		aPlusB          fr_bn254.Element
+		aTimesB         fr_bn254.Element
+		twoTimesATimesB fr_bn254.Element
+		xorResult       fr_bn254.Element
+	)
+	two := fr_bn254.NewElement(2)
+	aPlusB.Add(&secretVariables[lhs], &secretVariables[rhs])
+	aTimesB.Mul(&secretVariables[lhs], &secretVariables[rhs])
+	twoTimesATimesB.Mul(&two, &aTimesB)
+	// a + b - 2 * a * b
+	xorResult.Sub(&aPlusB, &twoTimesATimesB)
+	secretVariables = append(secretVariables, xorResult)
+
+	andConstraint := constraint.SparseR1C{
+		L: sparseR1CS.MakeTerm(&qL, xa),
+		R: sparseR1CS.MakeTerm(&qR, xb),
+		O: sparseR1CS.MakeTerm(&qO, xc),
+		M: [2]constraint.Term{sparseR1CS.MakeTerm(&qM1, xa), sparseR1CS.MakeTerm(&qM2, xb)},
+		K: constraint.CoeffIdZero,
+	}
+
+	sparseR1CS.AddConstraint(andConstraint)
+
+	return xc, secretVariables
+}
+
+// Generates constraints for the XOR operation between to values.
+// If you know beforehand that the inputs are boolean, you should use the `xor`
+// component.
+//
+// It generates 2 * ((3 * bits) + 1) + bits + (2 * bits) Plonk constraints.
+//
+// lhs is the index of the left hand side value in the values vector.
+// rhs is the index of the right hand side value in the values vector.
+// sparseR1CS is the constraint system being mutated.
+// secretVariables is the values vector.
+//
+// Returns a tuple with the index of the result of the AND operation in the values
+// vector and the updated values vector.
+func Xor(lhs int, rhs int, bits int, sparseR1CS *cs_bn254.SparseR1CS, secretVariables fr_bn254.Vector) (int, fr_bn254.Vector) {
+	lhsBitsIndices, secretVariables := toBinaryConversion(lhs, bits, sparseR1CS, secretVariables)
+	rhsBitsIndices, secretVariables := toBinaryConversion(rhs, bits, sparseR1CS, secretVariables)
+	resultBits := make([]int, bits)
+
+	for i := 0; i < bits; i++ {
+		lhsBitIndex := lhsBitsIndices[i]
+		rhsBitIndex := rhsBitsIndices[i]
+		// Inputs were constrained in the above `toBinaryConversion` calls.
+		resultBit, _secretVariables := xor(lhsBitIndex, rhsBitIndex, sparseR1CS, secretVariables, false)
 		secretVariables = _secretVariables
 		resultBits[i] = resultBit
 	}
